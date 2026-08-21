6f69d71ec42d803f592cb6000ae24725c2e1cd85
LF5 <lf524prod24061993@gmail.com>
2026-08-22 02:07:28 +0300

    Modify task utilization and migration parameters
    Updated minimum task utilization for boost and uclamp settings, and adjusted migration percentages.

--- kernel/sched/walt/sysctl.c
+++ kernel/sched/walt/sysctl.c
@@ -55,8 +55,8 @@ unsigned int sysctl_sched_task_unfilter_period;
 unsigned int __read_mostly sysctl_sched_asym_cap_sibling_freq_match_pct;
 unsigned int sysctl_walt_low_latency_task_threshold; /* disabled by default */
 unsigned int sysctl_sched_conservative_pl;
-unsigned int sysctl_sched_min_task_util_for_boost = 51;
-unsigned int sysctl_sched_min_task_util_for_uclamp = 51;
+unsigned int sysctl_sched_min_task_util_for_boost = 35;
+unsigned int sysctl_sched_min_task_util_for_uclamp = 35;
 unsigned int sysctl_sched_min_task_util_for_colocation = 35;
 unsigned int sysctl_sched_many_wakeup_threshold = WALT_MANY_WAKEUP_DEFAULT;
 const int sched_user_hint_max = 1000;
@@ -920,9 +920,9 @@ void walt_tunables(void)
 		sysctl_sched_capacity_margin_dn_pct[i] = 85; /* ~15% margin */
 	}
 
-	sysctl_sched_group_upmigrate_pct = 100;
+	sysctl_sched_group_upmigrate_pct = 95;
 
-	sysctl_sched_group_downmigrate_pct = 95;
+	sysctl_sched_group_downmigrate_pct = 85;
 
 	sysctl_sched_asym_cap_sibling_freq_match_pct = 100;
 
